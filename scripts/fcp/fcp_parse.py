6ffa3ace6839e1e0913b12acaeff7ee1729b313c
iliskhan <iliskhan234@gmail.com>
2019-11-23 19:30:26 +0300

    (update: requirements.txt) changed folders structure

---
+++ scripts/fcp/fcp_parse.py
@@ -0,0 +1,46 @@
+import requests
+
+from bs4 import BeautifulSoup as BS 
+
+
+def content_finder(url):
+
+    page_number = 1
+    
+    while True:
+
+        response = requests.get(f"{url}?page={page_number}&ajax=feed")
+
+        if response.status_code == 200:
+            soup = BS(response.text , features="html5lib")
+
+            news_block = soup.find(class_="news-block")
+            if news_block:
+                
+                headline = news_block.find_all("div", class_="headline")
+                for chunk in headline:
+                    print(chunk.find("time").text)
+                    print(chunk.find("a").text)
+                    print(chunk.find("span", class_="headline_title_link").text)
+                    headline_lead = chunk.find("span", class_="headline_lead")
+                    if headline_lead:
+                        print(headline_lead.text)
+            else:
+                break
+        else:
+            break
+        
+        page_number+=1
+
+
+def main():
+    url = "http://government.ru/rugovclassifier/860/events/"
+
+    content_finder(url)
+
+    # main_url = "http://government.ru/"
+
+if __name__ == "__main__":
+    main()
+
+    
