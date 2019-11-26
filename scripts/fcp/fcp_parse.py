426aa1393346d2bf4b26a0bb78df932cfaaf1fbb
iliskhan <iliskhan234@gmail.com>
2019-11-26 14:50:05 +0300

    (fix: scripts/fcp/fcp_parse.py) saved data to json

--- scripts/fcp/fcp_parse.py
+++ scripts/fcp/fcp_parse.py
@@ -1,9 +1,14 @@
+import json
 import requests
+
+from tqdm import tqdm
 
 from bs4 import BeautifulSoup as BS 
 
 
-def content_finder(url):
+def content_finder(url, main_url):
+
+    data = []
 
     page_number = 1
     
@@ -11,22 +16,28 @@ def content_finder(url):
 
         response = requests.get(f"{url}?page={page_number}&ajax=feed")
 
-        if response.status_code == 200:
+        if response.ok:
             soup = BS(response.text , features="html5lib")
 
             news_block = soup.find(class_="news-block")
             if news_block:
                 
                 headline = news_block.find_all("div", class_="headline")
+
+                current_content = {}
+
                 for chunk in headline:
-                    print(chunk.find("time").text)
-                    print(chunk.find("a").text)
-                    print(chunk.find("span", class_="headline_title_link").text)
+                    current_content['time'] = chunk.find("time").text
+                    current_content['card_name'] = chunk.find("a").text
+                    current_content['title'] = chunk.find("span", class_="headline_title_link").text
                     headline_lead = chunk.find("span", class_="headline_lead")
                     if headline_lead:
-                        print(headline_lead.text)
+                        current_content['lead'] = headline_lead.text
 
-                    print(chunk.find("a", class_="headline__link")['href'])
+                    url_chunk = chunk.find("a", class_="headline__link")['href']
+                    current_content['link'] = main_url + url_chunk
+
+                    data.append(current_content)
             else:
                 break
         else:
@@ -34,13 +45,37 @@ def content_finder(url):
         
         page_number+=1
 
+    title = requests.get(url)
+
+    if title.ok:
+        title = BS(title.text, features="html5lib")
+
+        title = title.find("p", class_="vcard_name vcard_name_selection").text
+        return {title: data}
+    
+
 
 def main():
-    url = "http://government.ru/rugovclassifier/860/events/"
+    urls = [
+        "http://government.ru/rugovclassifier/860/events/",
+        "http://government.ru/rugovclassifier/856/events/",
+        "http://government.ru/rugovclassifier/821/events/",
+        "http://government.ru/rugovclassifier/858/events/",
+        "http://government.ru/rugovclassifier/823/events/",
+        "http://government.ru/rugovclassifier/862/events/",
+        "http://government.ru/rugovclassifier/845/events/",
+        "http://government.ru/rugovclassifier/837/events/",
+        "http://government.ru/rugovclassifier/854/events/",
+        "http://government.ru/rugovclassifier/817/events/",
+        "http://government.ru/rugovclassifier/821/events/",
+        "http://government.ru/rugovclassifier/826/events/",
+        ]
 
-    content_finder(url)
+    main_url = "http://government.ru"
+    data = [content_finder(url, main_url) for url in tqdm(urls)]
 
-    # main_url = "http://government.ru/"
+    with open("data.json", "w", encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     main()
