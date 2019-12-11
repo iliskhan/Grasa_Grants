a0188244e4522159309872ca1adaf9839fffe06c
GG-Kh <hava1hava@mail.ru>
2019-12-11 17:50:44 +0300

    modified: add information from the FZ to the database

--- scripts/fz/retrive_data_from_fz223.py
+++ scripts/fz/retrive_data_from_fz223.py
@@ -1,20 +1,26 @@
 import os
-import json
-
+import sys
 import xml.etree.ElementTree as ET
+import datetime
+
+sys.path.append('../../grasagrant')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'grasagrant.settings'
+import django
+django.setup()
+
+from main.models import Category, Type, Fz223
 
 def retrieve(file_path):
 
     ns = {'xmlns':'http://zakupki.gov.ru/223fz/types/1',
           'ns2':'http://zakupki.gov.ru/223fz/purchase/1'}
-
-    data = dict()
-
+    
     xmlData223 = ET.parse(file_path)
 
     root = xmlData223.getroot()
-    
-    name = file_path.split('_')[1].split('/')[1]
+
+    name = file_path.split('_')[0].split('/')[-1]
 
     item = root.find('ns2:body/'
                      'ns2:item', ns)
@@ -38,122 +44,94 @@ def retrieve(file_path):
     if name == 'purchaseNoticeZPESMBO':
         purchaseNotice = item.find('ns2:purchaseNoticeZPESMBOData', ns)
 
+    pk = Category.objects.get(tab_name='Fz223').pk   
+    types = Type.objects.filter(category=pk)
 
-    data['registrationNumber'] = purchaseNotice.find('ns2:registrationNumber', ns).text
+    fz223 = Fz223()
 
-    data['createDateTime'] = purchaseNotice.find('ns2:createDateTime', ns).text
+    if name == 'purchaseNotice':
+       fz223.fz = types.get(name='Извещение о закупке "Иной способ"')
 
-    data['urlEIS'] = purchaseNotice.findtext('ns2:urlEIS', namespaces = ns)
-    data['urlVSRZ'] = purchaseNotice.findtext('ns2:urlVSRZ', namespaces = ns)
+    if name == 'purchaseNoticeAE':
+        fz223.fz = types.get(name='Извещение о закупке "Открытый аукцион в электронной форме"')
 
-    data['name'] = purchaseNotice.find('ns2:name', ns).text
+    if name == 'purchaseNoticeOA':
+        fz223.fz = types.get(name='Извещение о закупке "Открытый Аукцион"')
 
-    data['fullName'] = purchaseNotice.find('ns2:customer/'
+    if name == 'purchaseNoticeOK':
+        fz223.fz = types.get(name='Извещение о закупке "Открытый конкурс"')
+
+    if name == 'purchaseNoticeZK':
+        fz223.fz = types.get(name='Извещение о закупке "Запрос котировок"')
+
+    if name == 'purchaseNoticeAESMBO':
+        fz223.fz = types.get(name='Извещение о закупке "Аукцион в ЭФ, участниками которого могут являться только субъекты МСП"')
+
+    if name == 'purchaseNoticeZKESMBO':
+        fz223.fz = types.get(name='Извещение о закупке "Запрос котировок в ЭФ, участниками которого могут являться только субъекты МСП"')
+
+    if name == 'purchaseNoticeKESMBO':
+        fz223.fz = types.get(name='Извещение о закупке "Конкурс в ЭФ, участниками которого могут являться только субъекты МСП"')
+
+    if name == 'purchaseNoticeZPESMBO':
+        fz223.fz = types.get(name='Извещение о закупке "Запрос предложений в ЭФ, участниками которого могут являться только субъекты МСП"')  
+
+    fz223.registration_number = purchaseNotice.find('ns2:registrationNumber', ns).text
+
+    createdate = purchaseNotice.find('ns2:createDateTime', ns).text.split('T')[0]
+    createdate = datetime.datetime.strptime(createdate, '%Y-%m-%d').date()
+    
+    fz223.create_date = createdate
+
+    fz223.url_EIS = purchaseNotice.findtext('ns2:urlEIS', namespaces = ns)
+
+    fz223.ulr_VSRZ = purchaseNotice.findtext('ns2:urlVSRZ', namespaces = ns)
+
+    fz223.name = purchaseNotice.find('ns2:name', ns).text
+
+    fz223.full_name = purchaseNotice.find('ns2:customer/'
                                          'xmlns:mainInfo/'
                                          'xmlns:fullName', ns).text
 
-    data['legalAddress'] = purchaseNotice.find('ns2:customer/'
+    fz223.legal_address = purchaseNotice.find('ns2:customer/'
                                          'xmlns:mainInfo/'
                                          'xmlns:legalAddress', ns).text
 
-    data['purchaseCodeName'] = purchaseNotice.find('ns2:purchaseCodeName', ns).text
+    fz223.purchase_code_name = purchaseNotice.find('ns2:purchaseCodeName', ns).text
 
-    data['place'] = purchaseNotice.findtext('ns2:documentationDelivery/'
+    fz223.place = purchaseNotice.findtext('ns2:documentationDelivery/'
                                             'xmlns:place', namespaces=ns)
 
     lot = purchaseNotice.find('ns2:lots/'
                               'xmlns:lot/'
                               'xmlns:lotData', ns)
-    data['currency'] = lot.find('xmlns:currency/'
+
+    fz223.currency = lot.find('xmlns:currency/'
                                 'xmlns:code', ns).text
 
-    data['initialSum'] = lot.find('xmlns:initialSum', ns).text
+    fz223.initial_sum = lot.find('xmlns:initialSum', ns).text
 
-    data['submisionStartDate'] = purchaseNotice.findtext('ns2:applSubmisionStartDate', namespaces=ns)
+    fz223.submission_start_date = purchaseNotice.findtext('ns2:applSubmisionStartDate', namespaces=ns)
 
-    data['submissionCloseDateTime'] = purchaseNotice.findtext('ns2:submissionCloseDateTime', namespaces=ns)
+    fz223.submission_close_date = purchaseNotice.findtext('ns2:submissionCloseDateTime', namespaces=ns)
 
-    return data
+    fz223.save()
 
 
 def main():
-    path = '../../data/223/all_files/'
+    path = '../../data/223/'
 
-    path_AE223 = '../../data/223/data_AE223/'
-    path_fz223 = '../../data/223/data_fz223/'
-    path_OA223 = '../../data/223/data_OA223/'
-    path_OK223 = '../../data/223/data_OK223/'
-    path_ZK223 = '../../data/223/data_ZK223/'
-    path_AESMBO223 = '../../data/223/data_AESMBO223/'
-    path_ZPESMBO223 = '../../data/223/data_ZPESMBO223/'
-    path_KESMBO223 = '../../data/223/data_KESMBO223/'
-    path_ZKESMBO223 = '../../data/223/data_ZKESMBO223/'
-
-    data_AE223 = []
-    data_fz223 = []
-    data_OA223 = []
-    data_OK223 = []
-    data_ZK223 = []
-    data_AESMBO223 = []
-    data_ZPESMBO223 = []
-    data_KESMBO223 = []
-    data_ZKESMBO223 = []
+    Fz223.objects.all().delete()
 
     for i in os.listdir(path):
 
         file_path = os.path.join(path, i)
 
-        if i.endswith('.xml') and i.startswith('purchaseNotice_'):
-            data_fz223.append(retrieve(file_path))
-
-        if i.endswith('.xml') and i.startswith('purchaseNoticeAE'):
-            data_AE223.append(retrieve(file_path))
-
-        if i.endswith('.xml') and i.startswith('purchaseNoticeOA'):
-            data_OA223.append(retrieve(file_path))
-
-        if i.endswith('.xml') and i.startswith('purchaseNoticeOK'):
-            data_OK223.append(retrieve(file_path))
-
-        if i.endswith('.xml') and i.startswith('purchaseNoticeZK'):
-            data_ZK223.append(retrieve(file_path))
-
-        if i.endswith('.xml') and i.startswith('purchaseNoticeAESMBO'):
-            data_AESMBO223.append(retrieve(file_path))
-
-        if i.endswith('.xml') and i.startswith('purchaseNoticeZKESMBO'):
-            data_ZKESMBO223.append(retrieve(file_path))
-
-        if i.endswith('.xml') and i.startswith('purchaseNoticeKESMBO'):
-            data_KESMBO223.append(retrieve(file_path))
-
-        if i.endswith('.xml') and i.startswith('purchaseNoticeZPESMBO'):
-            data_ZPESMBO223.append(retrieve(file_path))
-
+        if i.endswith('.xml') and i.startswith('purchaseNotice'):
+            retrieve(file_path)
 
         if i != '.gitkeep':
             os.remove(file_path)
-
-
-    with open(f'{path_fz223}/info_fz223.json', 'w', encoding='utf8') as f:
-        json.dump(data_fz223, f, ensure_ascii=False, indent=4)
-    with open(f'{path_AE223}/info_AE223.json', 'w', encoding='utf8') as f:
-        json.dump(data_AE223, f, ensure_ascii=False, indent=4)
-    with open(f'{path_OA223}/info_OA223.json', 'w', encoding='utf8') as f:
-        json.dump(data_OA223, f, ensure_ascii=False, indent=4)
-    with open(f'{path_OK223}/info_OK223.json', 'w', encoding='utf8') as f:
-        json.dump(data_OK223, f, ensure_ascii=False, indent=4)
-    with open(f'{path_ZK223}/info_ZK223.json', 'w', encoding='utf8') as f:
-        json.dump(data_ZK223, f, ensure_ascii=False, indent=4)
-    with open(f'{path_AESMBO223}/info_AESMBO223.json', 'w', encoding='utf8') as f:
-        json.dump(data_AESMBO223, f, ensure_ascii=False, indent=4)
-    with open(f'{path_ZPESMBO223}/info_ZPESMBO223.json', 'w', encoding='utf8') as f:
-        json.dump(data_ZPESMBO223, f, ensure_ascii=False, indent=4)
-    with open(f'{path_KESMBO223}/info_KESMBO223.json', 'w', encoding='utf8') as f:
-        json.dump(data_KESMBO223, f, ensure_ascii=False, indent=4)
-    with open(f'{path_ZKESMBO223}/info_ZKESMBO223.json', 'w', encoding='utf8') as f:
-        json.dump(data_ZKESMBO223, f, ensure_ascii=False, indent=4)
-
 
 if __name__ == '__main__':
     main()
