f9478c74effe24c318a63d06a1c89b0bc8d12ca7
GG-Kh <hava1hava@mail.ru>
2019-12-11 19:11:40 +0300

    (modified: main/models.py) add to some fields null = True

--- grasagrant/main/models.py
+++ grasagrant/main/models.py
@@ -6,10 +6,13 @@ class Category(models.Model):
 
     tab_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.tab_name
+
 
 class Type(models.Model):
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=300)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="type")
 
     def __str__(self):
@@ -20,22 +23,21 @@ class Fz223(models.Model):
     fz = models.ForeignKey(Type, on_delete=models.CASCADE, related_name="fz223")
     registration_number = models.CharField(max_length=100)
     create_date = models.DateField()
-    url_EIS = models.TextField()
-    ulr_VSRZ = models.TextField()
+    url_EIS = models.TextField(null=True)
+    ulr_VSRZ = models.TextField(null=True)
     name = models.CharField(max_length=300)
     full_name = models.CharField(max_length=300)
     legal_address = models.CharField(max_length=300)
     purchase_code_name = models.CharField(max_length=300)
-    place = models.CharField(max_length=300)
+    place = models.CharField(max_length=300, null=True)
     currency = models.CharField(max_length=100)
     initial_sum = models.CharField(max_length=100)
-    submission_start_date = models.CharField(max_length=100)
-    submission_close_date = models.CharField(max_length=100)
+    submission_start_date = models.CharField(max_length=100, null=True)
+    submission_close_date = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return f'{self.fz.__str__()}:{self.name}'
+        return f'{self.fz.__str__()}: {self.name}'
     
-
 class Fz44(models.Model):
 
     fz = models.ForeignKey(Type, on_delete=models.CASCADE, related_name="fz44")
@@ -44,17 +46,20 @@ class Fz44(models.Model):
     purchase_number = models.CharField(max_length=100)
     purchase_object = models.CharField(max_length=300)
     org_name = models.CharField(max_length=100)
-    placing_way = models.CharField(max_length=100)
-    start_date = models.CharField(max_length=100)
-    end_date = models.CharField(max_length=100)
-    place = models.TextField()
+    placing_way = models.CharField(max_length=100, null=True)
+    start_date = models.CharField(max_length=100, null=True)
+    end_date = models.CharField(max_length=100, null=True)
+    place = models.TextField(null=True)
     max_price = models.CharField(max_length=100)
     currency = models.CharField(max_length=100)
     finance_source = models.CharField(max_length=100)
-    aplication_guarantee = models.CharField(max_length=100)
-    contract_guarantee = models.CharField(max_length=100)
-    delivery = models.CharField(max_length=300)
-    delivery_place = models.CharField(max_length=300)
+    aplication_guarantee = models.CharField(max_length=100, null=True)
+    contract_guarantee = models.CharField(max_length=100, null=True)
+    delivery = models.CharField(max_length=300, null=True)
+    delivery_place = models.CharField(max_length=300, null=True)
+
+    def __str__(self):
+        return f'{self.fz.__str__()}: {self.purchase_object}'
 
 class Fcp(models.Model):
 
@@ -67,21 +72,25 @@ class Fcp(models.Model):
 
 class Grant(models.Model):
     grant_name = models.ForeignKey(Type, on_delete=models.CASCADE, related_name="grants")
-    time = models.DateField(blank=True)
-    label = models.CharField(max_length=100, blank=True)
-    text = models.TextField(blank=True)
-    link = models.CharField(max_length=300, blank=True, )
-    org = models.CharField(max_length=300, blank=True)
-    days = models.CharField(max_length=30, blank=True)
-    rouble = models.CharField(max_length=30, blank=True)
-    fond = models.CharField(max_length=300, blank=True)
-    fond_link = models.CharField(max_length=300, blank=True)
+    time = models.DateField()
+    label = models.CharField(max_length=100, null=True)
+    text = models.TextField()
+    link = models.CharField(max_length=300, null=True)
+    org = models.CharField(max_length=300, null=True)
+    days = models.CharField(max_length=30, null=True)
+    rouble = models.CharField(max_length=30, null=True)
+    fond = models.CharField(max_length=300, null=True)
+    fond_link = models.CharField(max_length=300, null=True)
 
     def __str__(self):
-        return f'{self.grant_name.__str__()}: {self.id}'
+        return f'{self.grant_name.__str__()}: {self.text}'
 
 
 class Link(models.Model):
 
     link = models.CharField(max_length=300)
     grant_id = models.ForeignKey(Grant, on_delete=models.CASCADE, related_name="links")
+
+    def __str__(self):
+        return self.grant_id.__str__()
+
