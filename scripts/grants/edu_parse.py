fc6c86b5d35b79592df1ee5fed85d500c6bfb302
GG-Kh <hava1hava@mail.ru>
2019-12-12 15:34:26 +0300

    (modified: grant/edu_parse.py grant/fcpir_parse.py) add grant information to the database

--- scripts/grants/edu_parse.py
+++ scripts/grants/edu_parse.py
@@ -1,16 +1,29 @@
 from requests_html import HTMLSession
-import json
-from bs4 import BeautifulSoup as BS 
+from bs4 import BeautifulSoup as BS
+import sys
+import os
+import datetime
+import locale
+
+sys.path.append('../../grasagrant')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'grasagrant.settings'
+import django
+django.setup()
+
+from main.models import Category, Type, Grant
 
 def edu_parse(url):
-
-    data = []
 
     session = HTMLSession()
 
     response = session.get('https://docs.edu.gov.ru/')
 
     if response.status_code == 200:
+
+        type_grant = Type.objects.get(name='edu')
+
+        Grant.objects.filter(grant_name=type_grant)
 
         response.html.render()
 
@@ -19,24 +32,36 @@ def edu_parse(url):
         doc_list = soup.find_all("div", class_="section page-main__searchresult-item")
 
         for doc in doc_list:
-            content = {}
 
-            content['time'] = doc.find('div', class_='date page-main__searchresult-item-meta-date mr-2 pr-2').text.strip()
-            content['text'] = doc.find('a').text.strip()
-            content['link'] = doc.find('a')['href']
-            content['label'] = doc.find('div', class_='d-flex').find_all('div')[1].text.strip()
+            grant = Grant()
 
-            data.append(content)
+            grant.grant_name = type_grant
 
-    return data
+            locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+            time = doc.find('div', class_='date page-main__searchresult-item-meta-date mr-2 pr-2').text.strip()
+            time = time.lower().split(' ')
+
+            if time[1].startswith('м') and time[1].endswith('я'):
+                time[1] = time[1].replace('я', 'й')
+            elif time[1].endswith('а'):
+                time[1] = time[1][:-1]
+            else:
+                time[1] = time[1][:-1] + 'ь'
+
+            time = ' '.join(i for i in time)          
+            
+            grant.time = datetime.datetime.strptime(time, '%d %B %Y').date()
+            grant.text = doc.find('a').text.strip()
+            grant.link = doc.find('a')['href']
+            grant.label = doc.find('div', class_='d-flex').find_all('div')[1].text.strip()
+
+            grant.save()
 
 def main():
 
     url = 'https://docs.edu.gov.ru/'
 
-    with open("data_edu.json", "w", encoding='utf-8') as f:
-        json.dump(edu_parse(url), f, ensure_ascii=False, indent=4)
-
+    edu_parse(url)
 
 if __name__ == '__main__':
     main()    
--- scripts/grants/fcpir_parse.py
+++ scripts/grants/fcpir_parse.py
@@ -1,14 +1,27 @@
 import requests
-import json
-from bs4 import BeautifulSoup as BS 
+from bs4 import BeautifulSoup as BS
+import sys
+import os
+import datetime
+import locale
+
+sys.path.append('../../grasagrant')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'grasagrant.settings'
+import django
+django.setup()
+
+from main.models import Category, Type, Grant
 
 def parse_fcpir(url):
-
-    data = []
 
     response = requests.get(url)
 
     if response.status_code == 200:
+
+        type_grant = Type.objects.get(name='fcpir')
+
+        Grant.objects.filter(grant_name=type_grant).delete()
         
         soup = BS(response.text, features='html5lib')
 
@@ -16,23 +29,34 @@ def parse_fcpir(url):
 
         for i in tr:
 
-            content = {}
-            content['time'] = i.find('span', class_='gray').text
-            content['text'] = i.find('a').text
-            content['link'] = 'http://www.fcpir.ru' + i.find('a')['href']
+            grant = Grant()
 
-            data.append(content)
+            grant.grant_name = type_grant
+            
+            locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+            time = i.find('span', class_='gray').text
+            time = time.lower().split(' ')
 
-    return data
-    
+            if time[1].startswith('м') and time[1].endswith('я'):
+                time[1] = time[1].replace('я', 'й')
+            elif time[1].endswith('а'):
+                time[1] = time[1][:-1]
+            else:
+                time[1] = time[1][:-1] + 'ь'
+
+            time = ' '.join(i for i in time)
+
+            grant.time = datetime.datetime.strptime(time, '%d %B %Y').date()
+            grant.text = i.find('a').text
+            grant.link = 'http://www.fcpir.ru' + i.find('a')['href']
+
+            grant.save()
 
 def main():
     
     url = 'http://www.fcpir.ru/events_and_publications/_contest/'
 
-    with open("data_fcpir.json", "w", encoding='utf-8') as f:
-        json.dump(parse_fcpir(url), f, ensure_ascii=False, indent=4)
-
+    parse_fcpir(url)
 
 if __name__ == '__main__':
     main()
