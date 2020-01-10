305f762c3572c856adbd38254484c174c672d786
iliskhan <iliskhan234@gmail.com>
2020-01-10 20:23:36 +0300

    implemented filtering by region on js

--- grasagrant/main/static/js/regions_filter.js
+++ grasagrant/main/static/js/regions_filter.js
@@ -4,6 +4,10 @@ const regions = document.getElementsByClassName('info-card')
 input.oninput = (event) => {
     let user_input = event.target.value
     let regions_list = [...regions].map(item => item.id)
-    console.log(regions_list)
-
+    let mask = regions_list.map(region => {
+        let splitted_region = region.split(' ')
+        let presence = splitted_region.map(str => str.startsWith(user_input.toLowerCase()) ? true: false)
+        return presence.includes(true)
+    })
+    mask.findIndex((item, idx) => {item ? regions[idx].style.display='block': regions[idx].style.display='none'});
 }
