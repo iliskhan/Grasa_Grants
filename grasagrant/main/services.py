48658cb2d7376c54e218b656c773ca169c3d8482
GG-Kh <hava1hava@mail.ru>
2020-01-18 15:05:17 +0300

    retrieve data from all pages and clear data by date

---
+++ grasagrant/main/services.py
@@ -0,0 +1,92 @@
+import os, sys
+import datetime
+
+sys.path.append('../')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'grasagrant.settings'
+import django
+django.setup()
+
+from main.models import Type, Grant, Fcp, Fz44, Fz223, DigitalEconomy, Category, Region
+
+class CleanGrant():
+
+    @staticmethod
+    def clean_4science(type_grant):
+
+        all_grants = Grant.objects.filter(grant_name=type_grant)
+
+        for grant in all_grants:
+
+            if grant.days:
+                day = int(grant.days)
+
+                if day - 1 < 0:
+                    grant.delete()
+                else:
+                    grant.days = day - 1
+                    grant.save()
+
+    @staticmethod
+    def clean(type_grant):
+
+        all_grants = Grant.objects.filter(grant_name=type_grant)
+        
+        for grant in all_grants:
+
+            if grant.time + datetime.timedelta(days=183) < datetime.date.today():
+                
+                grant.delete()
+                
+class CleanFZ():
+
+    @staticmethod
+    def clean_fz44():
+
+        all_fz = Fz44.objects.all()
+
+        for fz in all_fz:
+            
+            if fz.end_date:
+
+                date = datetime.datetime.strptime(fz.end_date, '%Y-%m-%d').date()
+
+                if date < datetime.date.today():
+                    
+                    fz.delete()
+    
+    @staticmethod
+    def clean_fz223():
+
+        all_fz = Fz223.objects.all()
+
+        for fz in all_fz:
+            
+            if fz.submission_close_date:
+
+                date = datetime.datetime.strptime(fz.submission_close_date, '%Y-%m-%d').date()
+
+                if date < datetime.date.today():
+                    
+                    fz.delete()
+            
+
+class CleanDigitalEconomy():
+
+    @staticmethod
+    def clean():
+
+        all_digital = DigitalEconomy.objects.all()
+
+        for digital in all_digital:
+
+            if digital.date:
+
+                if digital.date + datetime.timedelta(days=183) < datetime.date.today():
+                    
+                    digital.delete()
+          
+
+
+
+
--- scripts/grants/grants_parser.py
+++ scripts/grants/grants_parser.py
@@ -14,237 +14,310 @@ django.setup()
 
 from main.models import Category, Type, Grant, Link
 
+from main.services import CleanGrant
+
 def science_parse(url):
 
-    response = requests.get(url, verify=False)
+    page_number = 1
 
-    if response.status_code == 200:
+    type_grant = Type.objects.get(name='4science')
+    CleanGrant.clean_4science(type_grant)
 
-        type_grant = Type.objects.get(name='4science')
+    while True:
 
-        all_grants = Grant.objects.filter(grant_name=type_grant)
+        response = requests.get(f'{url}?view=list&page={page_number}', verify=False)
 
-        for grant in all_grants:
-            Grant.clean_correct_days_4science(grant)
+        if response.status_code == 200:
 
-        soup = BS(response.text, features='html5lib')
-
-        posts = soup.find('div', id='fin-supports-list-inner').find_all('a', class_='fin-list')
+            soup = BS(response.text, features='html5lib')
+            posts = soup.find('div', id='fin-supports-list-inner').find_all('a', class_='fin-list')
+            
+            if posts:
         
-        for post in posts:
+                for post in posts:
 
-            time = post.find('span', class_='fin-list-date').text
-            div_text = post.find('div', class_='fin-list-title')
-            div_text.span.decompose()
-            text = div_text.text.strip()
+                    time = post.find('span', class_='fin-list-date').text
+                    time = datetime.datetime.strptime(time, '%d.%m.%Y').date()
+                    div_text = post.find('div', class_='fin-list-title')
+                    div_text.span.decompose()
+                    text = div_text.text.strip()
 
-            if Grant.objects.filter(text=text).exists() is not True:
-                
-                grant = Grant()
-
-                grant.grant_name = type_grant
-
-                grant.time = datetime.datetime.strptime(time, '%d.%m.%Y').date()
-
-                grant.label = post.find('div', class_='fin-list-label').text.strip()
-                
-                grant.text = text          
-                grant.link = 'https://4science.ru' + post['href']
-
-                info = post.find('div', class_='fin-list-info')
-
-                org = info.find_all('div', class_='fin-list-info-in-last')
-                if org:
-                    grant.org = org[0].text.strip()
-
-                for i in org:
-                    i.decompose()    
-
-                days = info.find('div', class_='fin-list-location')
-                if days:
-                    grant.days = days.text.strip()
-
-                days.decompose()
-
-                rouble = info.find('div', class_='fin-list-info-in')
-                
-                if rouble:
-                    rouble.span.decompose()
-                    grant.rouble = rouble.text.strip()
+                    if Grant.objects.filter(grant_name=type_grant, text=text, time=time).exists():                     
+                        return
                     
-                grant.save()
+                    grant = Grant()
+
+                    grant.grant_name = type_grant
+                    grant.time = time
+                    grant.label = post.find('div', class_='fin-list-label').text.strip()
+                    grant.text = text          
+                    grant.link = 'https://4science.ru' + post['href']
+
+                    info = post.find('div', class_='fin-list-info')
+                    org = info.find_all('div', class_='fin-list-info-in-last')
+                    if org:
+                        grant.org = org[0].text.strip()
+
+                    for i in org:
+                        i.decompose()    
+
+                    days = info.find('div', class_='fin-list-location')
+                    if days:
+                        grant.days = days.text.strip().split()[0]
+                        days.decompose()
+
+                    rouble = info.find('div', class_='fin-list-info-in')
+                    
+                    if rouble:
+                        rouble.span.decompose()
+                        grant.rouble = rouble.text.strip()
+                        
+                    grant.save()
+
+            else:
+                break
+        else:
+            break
+
+        page_number += 1
 
 
 def cbias_parse(url):
 
-    response = requests.get(url)
+    page_number = 1
 
-    if response.status_code == 200:
+    type_grant = Type.objects.get(name='cbias')
+    CleanGrant.clean(type_grant)
 
-        type_grant = Type.objects.get(name='cbias')
-        Grant.objects.filter(grant_name=type_grant).delete()
+    while True:
 
-        soup = BS(response.text, features='html5lib')
-        posts = soup.find_all(class_='post_inner')
+        response = requests.get(f'{url}page/{page_number}/')
+        
+        if response.status_code == 200:
 
-        for post in posts:
+            soup = BS(response.text, features='html5lib')
+            posts = soup.find_all(class_='post_inner')
 
-            grant = Grant()
+            if posts:
 
-            grant.grant_name = type_grant
-            time = post.find('div', class_='post_date').text
-            grant.time = datetime.datetime.strptime(time, '%d.%m.%Y').date()
-            
-            text = post.find_all('p')
-            grant.text = ' '.join(i.text.strip() for i in text)
-            
-            grant.save() 
+                for post in posts:
 
-            doc_link = post.find_all('p')
-            doc_link = [i.find('a') for i in doc_link]
-            
-            for doc in doc_link:
-                if doc is not None: 
-
-                    doc = doc['href']
+                    text = post.find_all('p')
+                    text = ' '.join(i.text.strip() for i in text)
+                    time = post.find('div', class_='post_date').text
+                    time = datetime.datetime.strptime(time, '%d.%m.%Y').date()     
                     
-                    if doc.endswith('.pdf') or doc.endswith('.doc'):
+                    if Grant.objects.filter(grant_name=type_grant, text=text, time=time).exists():
 
-                        link = Link()
-                        link.grant_id = grant
-                        link.link = doc
-                        
-                        link.save()             
+                        return
+                
+                    grant = Grant()
 
+                    grant.grant_name = type_grant                   
+                    grant.time = time     
+                    grant.text = text                   
+                    grant.save() 
+
+                    doc_link = post.find_all('p')
+                    doc_link = [i.find('a') for i in doc_link]
+                    
+                    for doc in doc_link:
+                        if doc is not None: 
+
+                            doc = doc['href']
+                            
+                            if doc.endswith('.pdf') or doc.endswith('.doc'):
+
+                                link = Link()
+                                link.grant_id = grant
+                                link.link = doc                               
+                                link.save()
+            else:
+                break
+        else:
+            break
+    
+        page_number += 1
 
 def fcpir_parse(url):
 
-    response = requests.get(url)
+    page_number = 1
 
-    if response.status_code == 200:
+    type_grant = Type.objects.get(name='fcpir')
+    CleanGrant.clean(type_grant)
 
-        type_grant = Type.objects.get(name='fcpir')
-        Grant.objects.filter(grant_name=type_grant).delete()
+    while True:
+
+        response = requests.get(f'{url}?PAGEN_1={page_number}')
         
-        soup = BS(response.text, features='html5lib')
-        tr = soup.find('table', class_='contest').find_all('tr')
+        if response.status_code == 200:
+        
+            soup = BS(response.text, features='html5lib')
+            tr = soup.find('table', class_='contest').find_all('tr')
 
-        for i in tr:
+            for i in tr:
 
-            grant = Grant()
-            grant.grant_name = type_grant
-            
-            time = i.find('span', class_='gray').text
-            grant.time = date_conversion(time)
-            grant.text = i.find('a').text
-            grant.link = 'http://www.fcpir.ru' + i.find('a')['href']
+                text = i.find('a').text
+                time = i.find('span', class_='gray').text
+                time = date_conversion(time)
 
-            grant.save()
+                if Grant.objects.filter(grant_name=type_grant, text=text, time=time).exists():
+                    return
+                
+                grant = Grant()
+                grant.grant_name = type_grant
+                
+                grant.time = time
+                grant.text = text
+                grant.link = 'http://www.fcpir.ru' + i.find('a')['href']
+                grant.save()
+
+            pagination = soup.find('div', class_='pagination')           
+            if pagination.find('span', class_='pagination__next _disabled') is not None:
+                break
+        else:
+            break
+
+        page_number += 1
 
 
 def minobrnauki_parse(url):
 
-    response = requests.get(url, verify=False)
-    
-    if response.status_code == 200:
+    page_number = 1
 
-        type_grant = Type.objects.get(name='minobrnauki')
-        Grant.objects.filter(grant_name=type_grant).delete()
-        
-        soup = BS(response.text, features='html5lib') 
-        li = soup.find("ul", class_="anons-list_docs").find_all("li")
+    type_grant = Type.objects.get(name='minobrnauki')
+    CleanGrant.clean(type_grant)
 
-        if li:
+    while True:
 
-            for i in li:
+        response = requests.get(f'{url}?order_4=P_DATE&dir_4=DESC&page_4={page_number}', verify=False)
+        print(f'{url}?order_4=P_DATE&dir_4=DESC&page_4={page_number}')
+        if response.status_code == 200:
+
+            soup = BS(response.text, features='html5lib') 
+            li = soup.find("ul", class_="anons-list_docs").find_all("li")
+
+            if li:
                 
-                grant = Grant()
-                grant.grant_name = type_grant
+                for i in li:
 
-                time = i.find("time").text
-                grant.time = datetime.datetime.strptime(time, '%d.%m.%Y').date()
-                grant.link = 'https://www.minobrnauki.gov.ru' + i.find("a")['href']
-                grant.text = i.find("a").text
+                    text = i.find("a").text
+                    time = i.find("time").text
+                    time = datetime.datetime.strptime(time, '%d.%m.%Y').date()
+                    link = 'https://www.minobrnauki.gov.ru' + i.find("a")['href']
 
-                grant.save()
+                    if Grant.objects.filter(grant_name=type_grant, text=text, time=time, link=link).exists():
+                        return
 
-                doc_links = i.find('div', class_='doc-links')
-                
-                if doc_links is not None:
-                    
-                    for i in doc_links.find_all('a'):
+                    else:
+                        grant = Grant()
+                        grant.grant_name = type_grant
+                        grant.time = time
+                        grant.link = link
+                        grant.text = text
+                        grant.save()
+
+                        doc_links = i.find('div', class_='doc-links')
                         
-                        link = Link()
-                        link.link = 'https://www.minobrnauki.gov.ru'+i['href']
-                        link.grant_id = grant
+                        if doc_links is not None:
+                            
+                            for i in doc_links.find_all('a'):
+                                
+                                link = Link()
+                                link.link = 'https://www.minobrnauki.gov.ru'+i['href']
+                                link.grant_id = grant
+                                link.save()
+            else:
+                break
+        else:
+            break
 
-                        link.save()
-
+        page_number += 1
 
 def rsci_parse(url):
 
-    response = requests.get(url)
+    page_number = 1
 
-    if response.status_code == 200:
+    type_grant = Type.objects.get(name='rsci')
+    CleanGrant.clean(type_grant)
 
-        type_grant = Type.objects.get(name='rsci')
-        Grant.objects.filter(grant_name=type_grant).delete()
+    today_date = datetime.date.today() - datetime.timedelta(days=1)
 
-        soup = BS(response.text, features='html5lib')
-        posts = soup.find_all(class_='col l4 m6 s12')
+    while True:
+
+        response = requests.get(f'{url}?PAGEN_1={page_number}&SIZEN_1=9')
+
+        if response.status_code == 200:
+
+            soup = BS(response.text, features='html5lib')
+            posts = soup.find_all(class_='col l4 m6 s12')
         
-        for post in posts:
+            for post in posts:
 
-            grant = Grant()
-            grant.grant_name = type_grant
+                text = post.find('div', 'info-card-deskription').find('h4', class_='text-title').text
+                time = ''.join(f"{post.find_all('span')[0].text}.{post.find_all('span')[1].text}")
+                time = datetime.datetime.strptime(time, '%d.%m.%Y').date()
+    
+                if time >= today_date and Grant.objects.filter(grant_name=type_grant, text=text).exists() is not True:
+                    
+                    grant = Grant()
+                    grant.grant_name = type_grant
 
-            time = ''.join(f"{post.find_all('span')[0].text}.{post.find_all('span')[1].text}")
+                    grant.time = time
+                    grant.text = text  
+                    grant.link = 'http://www.rsci.ru' + post.find('div', 'info-card-deskription').find('a')['href']
+                    
+                    fond = post.find('div', class_='info-title')
+                    if fond:
+                        grant.fond = fond.text
+                    
+                    fond_link = post.find('div', class_='info-title').find('a')
+                    if fond_link:
+                        grant.fond_link = 'http://www.rsci.ru' + fond_link['href']
+                        
+                    grant.save()
 
-            grant.time = datetime.datetime.strptime(time, '%d.%m.%Y').date()
-            grant.text = post.find('div', 'info-card-deskription').find('h4', class_='text-title').text  
-            grant.link = 'http://www.rsci.ru' + post.find('div', 'info-card-deskription').find('a')['href']
-            
-            fond = post.find('div', class_='info-title')
-            if fond:
-                grant.fond = fond.text
-            
-            fond_link = post.find('div', class_='info-title').find('a')
-            if fond_link:
-                grant.fond_link = 'http://www.rsci.ru' + fond_link['href']
-                
-            grant.save()      
+                else: return
+        else:
+            break
 
+        page_number += 1
 
 def edu_parse(url):
 
     session = HTMLSession()
     response = session.get(url)
 
+    type_grant = Type.objects.get(name='edu')
+    CleanGrant.clean(type_grant)
+
+    today_date = datetime.date.today() - datetime.timedelta(days=1)
+
     if response.status_code == 200:
 
-        type_grant = Type.objects.get(name='edu')
-        Grant.objects.filter(grant_name=type_grant).delete()
-
-        response.html.render(timeout=30)
+        response.html.render(timeout=10)
 
         soup = BS(response.html.html, features="lxml")
         doc_list = soup.find_all("div", class_="section page-main__searchresult-item")
 
         for doc in doc_list:
 
-            grant = Grant()
-            grant.grant_name = type_grant
+            text = doc.find('a').text.strip()
+            time = doc.find('div', class_='date page-main__searchresult-item-meta-date mr-2 pr-2').text.strip()
+            time = date_conversion(time)
 
-            time = doc.find('div', class_='date page-main__searchresult-item-meta-date mr-2 pr-2').text.strip()      
-            grant.time = date_conversion(time)
+            if time >= today_date and Grant.objects.filter(grant_name=type_grant, text=text, time=time).exists() is not True:
+                
+                grant = Grant()
+                grant.grant_name = type_grant     
+                grant.time = time
+                grant.text = text
+                grant.link = doc.find('a')['href']
+                grant.label = doc.find('div', class_='d-flex').find_all('div')[1].text.strip()
 
-            grant.text = doc.find('a').text.strip()
-            grant.link = doc.find('a')['href']
-            grant.label = doc.find('div', class_='d-flex').find_all('div')[1].text.strip()
-
-            grant.save()
+                grant.save()
     
-    # session.close()    
+    session.close()    
      
 def date_conversion(date):
 
@@ -266,11 +339,11 @@ def date_conversion(date):
 
 def main():
 
-    science_parse('https://4science.ru/finsupports')
-    # cbias_parse('http://www.cbias.ru/')
+    # science_parse('https://4science.ru/finsupports') 
+    # cbias_parse('http://www.cbias.ru/category/news/')
     # fcpir_parse('http://www.fcpir.ru/events_and_publications/_contest/')
     # minobrnauki_parse('https://www.minobrnauki.gov.ru/ru/documents/docs/index.php')
-    # rsci_parse('http://www.rsci.ru/grants/') 
+    # rsci_parse('http://www.rsci.ru/grants/')
     # edu_parse('https://docs.edu.gov.ru/')  
 
 if __name__ == '__main__':
